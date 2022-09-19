9af1d3cf0f598aab2048f616df161eb07006b4bd
yungtisma <mpavlovi6@gmail.com>
2022-09-19 11:03:02 +0200

    fixed errors

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -30,5 +30,5 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_getitem.call_count, 6)
+        self.assertEqual(patched_getitem.call_count, 7)
 
